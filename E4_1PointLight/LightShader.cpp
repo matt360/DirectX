000f053491fe8a54f9622234096c476264214520
Matthew Wallace <1502616@abertay.ac.uk>
2017-10-18 15:02:56 +0100

    adding 4 lights into the scene E4_1PointLight E4_2PintLights

---
+++ E4_1PointLight/LightShader.cpp
@@ -0,0 +1,275 @@
+// texture shader.cpp
+#include "lightshader.h"
+
+// When resources are being created and interfaced with,
+// the device interface is used.
+LightShader::LightShader(ID3D11Device* device, HWND hwnd) : BaseShader(device, hwnd)
+{
+	// compiled shader object
+	initShader(L"light_vs.cso", L"light_ps.cso");
+}
+
+
+LightShader::~LightShader()
+{
+	// Release the sampler state.
+	if (sampleState)
+	{
+		sampleState->Release();
+		sampleState = 0;
+	}
+
+	// Release the matrix constant buffer.
+	if (matrixBuffer)
+	{
+		matrixBuffer->Release();
+		matrixBuffer = 0;
+	}
+
+	// Release the layout.
+	if (layout_)
+	{
+		layout_->Release();
+		layout_ = 0;
+	}
+
+	// Release the light constant buffer.
+	if (lightBuffer)
+	{
+		lightBuffer->Release();
+		lightBuffer = 0;
+	}
+
+	if (cameraBuffer)
+	{
+		cameraBuffer->Release();
+		cameraBuffer = 0;
+	}
+	//Release base shader components
+	BaseShader::~BaseShader();
+}
+
+
+void LightShader::initShader(WCHAR* vsFilename, WCHAR* psFilename)
+{
+	D3D11_BUFFER_DESC matrixBufferDesc;
+	D3D11_SAMPLER_DESC samplerDesc;
+	D3D11_BUFFER_DESC lightBufferDesc;
+	// Camera
+	D3D11_BUFFER_DESC cameraBufferDesc;
+
+	// Load (+ compile) shader files
+	loadVertexShader(vsFilename);
+	loadPixelShader(psFilename);
+
+	// Setup the description of the dynamic matrix constant buffer that is in the vertex shader.
+	matrixBufferDesc.Usage = D3D11_USAGE::D3D11_USAGE_DYNAMIC;
+	matrixBufferDesc.ByteWidth = sizeof(MatrixBufferType);
+	matrixBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	matrixBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	//matrixBufferDesc.MiscFlags = D3D11_RESOURCE_MISC_FLAG::;
+	matrixBufferDesc.MiscFlags = 0;
+	matrixBufferDesc.StructureByteStride = 0;
+
+	// Create the constant buffer pointer so we can access the vertex shader constant buffer from within this class.
+	// ID3D11Device interface is reposnsible for creating all memory resources
+	// The created resources can then be attached to the pipeline either directly or with a resource view,
+	// where they are then used during a pipeline execution event
+	// The resource creation process uses a different ID3D11Device method for each type of resource,
+	// but they all follow the same general pattern.
+
+	// The creation mathods all take three parameters. The first parameter is a structure that specifies 
+	// all of the various options that a resource can be created with.
+	// It is referred ti as a resource description. Each resource type uses its own description structure, since they each 
+	// have a different set of available properties, but the structures all serve the same purpose - 
+	// to define the desired characteristics of the created resource.
+
+	// The second parameter in the resource creation methods is a pointer to a D3D11_SUBRESOURCE_DATA structure,
+	// which is used to provied the initial data to be loaded inta a resource.
+	// For example, if a buffer resource will hold static verted data, this structure would be used to pass a models's
+	// vertex data into the buffer. This eliminates the need to manually manipulate the buffer after it is created,
+	// if its contents will not be changing. This parameter can also just be set to null if non initialization is required. 
+
+	// The final parameter is a pointer to a pointer to the appropriate resource interface,
+	// which is where the created resource pointer is stored after a successful resource creation event.
+
+	// In each if these methods, the real configuration ocurs in the resource description structure. As mentioned above,
+	// each resource type has its own structure used to define its properties. However, there are some common elements that are
+	// shared across all of the structures. These include the usage flags, bind flags, CPU access flags, and miscellaneous flags.
+
+                           // resource       resouce  a pointer to a pointer to the appropriate resource
+	                       // description,   creation 
+	                       //                method,
+	renderer->CreateBuffer(&matrixBufferDesc, NULL, &matrixBuffer);
+
+	// Create a texture sampler state description.
+	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_MIRROR;
+	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_MIRROR;
+	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_MIRROR;
+	samplerDesc.MipLODBias = 0.0f;
+	samplerDesc.MaxAnisotropy = 1;
+	samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+	samplerDesc.BorderColor[0] = 0;
+	samplerDesc.BorderColor[1] = 0;
+	samplerDesc.BorderColor[2] = 0;
+	samplerDesc.BorderColor[3] = 0;
+	samplerDesc.MinLOD = 0;
+	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+
+	// Create the texture sampler state.
+	renderer->CreateSamplerState(&samplerDesc, &sampleState);
+
+	// Setup light buffer
+	// Setup the description of the light dynamic constant buffer that is in the pixel shader.
+	// Note that ByteWidth always needs to be a multiple of 16 if using D3D11_BIND_CONSTANT_BUFFER or CreateBuffer will fail.
+	lightBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	lightBufferDesc.ByteWidth = sizeof(LightBufferType);
+	lightBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	lightBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	lightBufferDesc.MiscFlags = 0;
+	lightBufferDesc.StructureByteStride = 0;
+
+	// Create the constant buffer pointer so we can access the vertex shader constant buffer from within this class.
+	renderer->CreateBuffer(&lightBufferDesc, NULL, &lightBuffer);
+
+	// Camera buffer
+	cameraBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	cameraBufferDesc.ByteWidth = sizeof(CameraBufferType);
+	cameraBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cameraBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cameraBufferDesc.MiscFlags = 0;
+	cameraBufferDesc.StructureByteStride = 0;
+
+	renderer->CreateBuffer(&cameraBufferDesc, NULL, &cameraBuffer);
+}
+
+void LightShader::setShaderParameters(ID3D11DeviceContext* deviceContext, const XMMATRIX &worldMatrix, const XMMATRIX &viewMatrix, const XMMATRIX &projectionMatrix, ID3D11ShaderResourceView* texture, Light* light, Camera* camera)
+{
+	HRESULT result;
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	MatrixBufferType* dataPtr;
+	LightBufferType* lightPtr;
+	CameraBufferType* cameraPtr;
+	unsigned int bufferNumber;
+	XMMATRIX tworld, tview, tproj;
+
+	// Transpose the matrices to prepare them for the shader.
+	tworld = XMMatrixTranspose(worldMatrix);
+	tview = XMMatrixTranspose(viewMatrix);
+	tproj = XMMatrixTranspose(projectionMatrix);
+
+	// Lock the constant buffer so it can be written to.
+	result = deviceContext->Map(matrixBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+
+	// Get a pointer to the data in the constant buffer.
+	dataPtr = (MatrixBufferType*)mappedResource.pData;
+
+	// Copy the matrices into the constant buffer.
+	dataPtr->world = tworld;// worldMatrix;
+	dataPtr->view = tview;
+	dataPtr->projection = tproj;
+
+	// Unlock the constant buffer.
+	deviceContext->Unmap(matrixBuffer, 0);
+
+	// Set the position of the constant buffer in the vertex shader.
+	bufferNumber = 0;
+
+	// Now set the constant buffer in the vertex shader with the updated values.
+	deviceContext->VSSetConstantBuffers(bufferNumber, 1, &matrixBuffer);
+
+	// Additional //
+	// Camera
+	deviceContext->Map(cameraBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	cameraPtr = (CameraBufferType*)mappedResource.pData;
+	cameraPtr->cameraPosition = camera->getPosition();
+	cameraPtr->padding = 0.0f;
+	deviceContext->Unmap(cameraBuffer, 0);
+	bufferNumber = 1;
+	deviceContext->VSSetConstantBuffers(bufferNumber, 1, &cameraBuffer);
+
+	// Send light data to pixel shader
+	deviceContext->Map(lightBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	lightPtr = (LightBufferType*)mappedResource.pData;
+	lightPtr->ambient = light->getAmbientColour();
+	lightPtr->diffuse = light->getDiffuseColour();
+	lightPtr->direction = light->getDirection();
+	lightPtr->specularPower = light->getSpecularPower();
+	lightPtr->specular = light->getSpecularColour();
+	lightPtr->position = light->getPosition();
+	lightPtr->padding = 0.0f;
+	
+	//lightPtr->padding = 0.0f;
+	deviceContext->Unmap(lightBuffer, 0);
+	bufferNumber = 0;
+	deviceContext->PSSetConstantBuffers(bufferNumber, 1, &lightBuffer);
+
+	// Set shader texture resource in the pixel shader.
+	deviceContext->PSSetShaderResources(0, 1, &texture);
+}
+
+void LightShader::setShaderParameters(ID3D11DeviceContext* deviceContext, const XMMATRIX &worldMatrix, const XMMATRIX &viewMatrix, const XMMATRIX &projectionMatrix, ID3D11ShaderResourceView* texture, Light* light)
+{
+	HRESULT result;
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	MatrixBufferType* dataPtr;
+	LightBufferType* lightPtr;
+	unsigned int bufferNumber;
+	XMMATRIX tworld, tview, tproj;
+
+
+	// Transpose the matrices to prepare them for the shader.
+	tworld = XMMatrixTranspose(worldMatrix);
+	tview = XMMatrixTranspose(viewMatrix);
+	tproj = XMMatrixTranspose(projectionMatrix);
+
+	// Lock the constant buffer so it can be written to.
+	result = deviceContext->Map(matrixBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+
+	// Get a pointer to the data in the constant buffer.
+	dataPtr = (MatrixBufferType*)mappedResource.pData;
+
+	// Copy the matrices into the constant buffer.
+	dataPtr->world = tworld;// worldMatrix;
+	dataPtr->view = tview;
+	dataPtr->projection = tproj;
+
+	// Unlock the constant buffer.
+	deviceContext->Unmap(matrixBuffer, 0);
+
+	// Set the position of the constant buffer in the vertex shader.
+	bufferNumber = 0;
+
+	// Now set the constant buffer in the vertex shader with the updated values.
+	deviceContext->VSSetConstantBuffers(bufferNumber, 1, &matrixBuffer);
+
+	//Additional
+	// Send light data to pixel shader
+	deviceContext->Map(lightBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	lightPtr = (LightBufferType*)mappedResource.pData;
+	lightPtr->ambient = light->getAmbientColour();
+	lightPtr->diffuse = light->getDiffuseColour();
+	lightPtr->direction = light->getDirection();
+	lightPtr->specularPower = light->getSpecularPower();
+	lightPtr->specular = light->getSpecularColour();
+	//lightPtr->padding = 0.0f;
+	deviceContext->Unmap(lightBuffer, 0);
+	bufferNumber = 0;
+	deviceContext->PSSetConstantBuffers(bufferNumber, 1, &lightBuffer);
+
+	// Set shader texture resource in the pixel shader.
+	deviceContext->PSSetShaderResources(0, 1, &texture);
+}
+
+void LightShader::render(ID3D11DeviceContext* deviceContext, int indexCount)
+{
+	// Set the sampler state in the pixel shader.
+	deviceContext->PSSetSamplers(0, 1, &sampleState);
+
+	// Base render function.
+	BaseShader::render(deviceContext, indexCount);
+}
+
+
+
---
+++ E4_2PointLights/shaders/light_ps.hlsl
@@ -0,0 +1,69 @@
+// Light pixel shader
+// Calculate diffuse lighting for a single directional light (also texturing)
+
+Texture2D shaderTexture : register(t0);
+SamplerState SampleType : register(s0);
+
+cbuffer LightBuffer : register(cb0)
+{
+    float4 ambientColor;
+    float4 diffuseColor;
+    float3 lightDirection;
+    float specularPower;
+    float4 specularColor;
+    float3 lightPosition;
+};
+
+struct InputType
+{
+    float4 position : SV_POSITION;
+    float2 tex : TEXCOORD0;
+    float3 normal : NORMAL;
+	float3 position3D : TEXCOORD1;
+};
+
+float4 main(InputType input) : SV_TARGET
+{
+	float4 textureColor;
+	float3 lightDir;
+	float lightIntensity;
+	float4 color;
+    float attenuation;
+	
+	// Sample the pixel color from the texture using the sampler at this texture coordinate location.
+	textureColor = shaderTexture.Sample(SampleType, input.tex);
+	
+	// Set the default output color to the ambient light value for all pixels.
+	color = ambientColor;
+	
+    lightDir = -(lightPosition - input.position3D);
+
+    float distance = length(lightDir);
+
+    //lightDir /= distance;
+	
+    lightDir = normalize(lightDir);
+
+	// Calculate the amount of light on this pixel.
+	lightIntensity = saturate(dot(input.normal, -lightDir));
+	
+	if (lightIntensity > 0.0f)
+	{
+		// attenuation
+        attenuation = 1.0f / (1.0f + 0.125f * distance + 0.0f * pow(distance, 2));
+
+		// Determine the final diffuse color based on the diffuse color and the amount of light intensity and attenuation.
+		color += (diffuseColor * lightIntensity * attenuation);
+	
+		// Saturate the ambient and diffuse color.
+		color = saturate(color);
+	}
+	
+	// Multiply the texture pixel and the input color to get the textured result.
+	color = color * textureColor;
+	
+	//// Add the specular component last to the output color.
+	//color = saturate(color + specular);
+	
+	return color;
+}
